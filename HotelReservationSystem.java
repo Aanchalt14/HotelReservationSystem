3ebfb97a26b96a9f3b77f1a50ae1b2529df34030
Aanchalt14 <aanchalt71@gmail.com>
2021-01-27 13:03:39 +0530

    Add : Hotel name with its reviews

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -1,3 +1,22 @@
+class AddHotel {
+
+    void hotel1() {
+        String Hotelname="Ridgewood";
+        float Rating= 3.0F;
+
+    }
+
+    void hotel2() {
+        String Hotelname="Bridgewood";
+        float Rating=-4.0F;
+    }
+
+    void hotel3() {
+        String Hotelname="PlayWood";
+        Float Rating=5.0F;
+    }
+}
+
 public class HotelReservationSystem {
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation program");
